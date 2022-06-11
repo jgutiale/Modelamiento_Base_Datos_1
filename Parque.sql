e201eefa046c8284d5a29628ac2fc361e2f630ef
jgutiale <joe.gutierrez@duocuc.cl>
2022-06-11 13:45:34 +0000

    Nueva tabla, primary key

--- Parque.sql
+++ Parque.sql
@@ -1,8 +1,20 @@
-CREATE TABLE Cliente(
+CREATE TABLE Clientes(
     rut_cliente      NUMBER(11) NOT NULL,
-    dv CHAR(1)       NOT NULL,
+    dv               CHAR(1) NOT NULL,
     primer_nombre    VARCHAR2(250) NOT NULL,
     segungo_nombre   VARCHAR2(250),
     apellido_paterno VARCHAR2(250) NOT NULL,
     apellido_materno VARCHAR2(250),
 );
+
+ALTER TABLE Clientes ADD CONSTRAINT cliente_PK PRIMARY KEY (rut_cliente);
+
+CREATE TABLE Comunas(
+    id_comuna NUMBER NOT NULL,
+    nombre VARCHAR2(250) NOT NULL,
+    provincia_id NUMBER NOT NULL,
+)
+
+ALTER TABLE Comunas ADD CONSTRAINT comuna_PK PRIMARY KEY (id_comuna);
+ALTER TABLE Clientes ADD CONSTRAINT comuna_FK FOREIGN KEY (comuna_id);
+    REFERENCES Comunas (id_comuna);
