c111171b7327866b6a10e539585233629dbb8ea1
jgutiale <joe.gutierrez@duocuc.cl>
2022-06-11 13:24:44 +0000

    completar carpeta cliente

--- Parque.sql
+++ Parque.sql
@@ -1,6 +1,8 @@
 CREATE TABLE Cliente(
-    rut_cliente     NUMBER(11) NOT NULL,
-    dv CHAR(1)      NOT NULL,
-    primer_nombre   VARCHAR2(250) NOT NULL,
-    segungo_nombre  VARCHAR2(250),
+    rut_cliente      NUMBER(11) NOT NULL,
+    dv CHAR(1)       NOT NULL,
+    primer_nombre    VARCHAR2(250) NOT NULL,
+    segungo_nombre   VARCHAR2(250),
+    apellido_paterno VARCHAR2(250) NOT NULL,
+    apellido_materno VARCHAR2(250),
 );
