5e708d5c17c5f4ced8cfd9984be59e1d41de4d8c
jgutiale <joe.gutierrez@duocuc.cl>
2022-06-30 03:39:23 +0000

    Agregado de insert

--- Evaluación 3.sql
+++ Evaluación 3.sql
@@ -11,7 +11,7 @@ CREATE TABLE Regiones (
 );
 
 CREATE TABLE Provincias (
-   Id_Provincia            NUMBER       NOT NULL,
+   Id_Provincia         NUMBER       NOT NULL,
    Nombre               CHAR (250)   NOT NULL,
    Regiones_Id_Region   NUMBER       NOT NULL
 );
@@ -244,3 +244,104 @@ ALTER TABLE Beneficios_Sindicato ADD CONSTRAINT Beneficios_Sindicato_Beneficios_
 ALTER TABLE Beneficios_Sindicato ADD CONSTRAINT Beneficios_Sindicato_Sindicatos_FK FOREIGN KEY (Sindicatos_Id_Sindicato) REFERENCES Sindicatos (Id_Sindicato);
 ALTER TABLE Afiliados_Cargas ADD CONSTRAINT Afiliados_Cargas_Afiliados_FK FOREIGN KEY (Afiliados_Id_Afiliado) REFERENCES Afiliados (Id_Afiliado);
 ALTER TABLE Modelos ADD CONSTRAINT Modelos_Marcas_FK FOREIGN KEY (Marcas_Id_Marca) REFERENCES Marcas (Id_Marca);
+
+--Formato fecha
+ALTER SESSION SET NLS_DATE_FORMAT = 'DD/MM/YY';
+
+--Insert
+INSERT INTO Paises VALUES (1, 'Chile');
+
+INSERT INTO Regiones VALUES (1,'Tarapacá',1);
+INSERT INTO Regiones VALUES (2,'Antofagasta',1);
+INSERT INTO Regiones VALUES (3,'Atacama',1);
+INSERT INTO Regiones VALUES (4,'Coquimbo',1);
+INSERT INTO Regiones VALUES (5,'Valparaíso',1);
+INSERT INTO Regiones VALUES (6,'O Higgins',1);
+INSERT INTO Regiones VALUES (7,'Maule',1);
+INSERT INTO Regiones VALUES (8,'Bío Bío',1);
+INSERT INTO Regiones VALUES (9,'Araucanía',1);
+INSERT INTO Regiones VALUES (10,'Los Lagos',1);
+INSERT INTO Regiones VALUES (11,'Aysén',1);
+INSERT INTO Regiones VALUES (12,'Magallanes',1);
+INSERT INTO Regiones VALUES (13,'Región Metropolitana de Santiago',1);
+INSERT INTO Regiones VALUES (14,'Los Ríos',1);
+INSERT INTO Regiones VALUES (15,'Arica y Parinacota',1);
+INSERT INTO Regiones VALUES (16,'Ñuble',1);
+
+INSERT INTO Provincias VALUES (1,'Provincia de Iquique',1);
+INSERT INTO Provincias VALUES (2,'Provincia de Antofagasta',2);
+INSERT INTO Provincias VALUES (3,'Provincia de Copiapo',3);
+INSERT INTO Provincias VALUES (4,'Provincia de Elqui',4);
+INSERT INTO Provincias VALUES (5,'Provincia de Valparaiso',5);
+INSERT INTO Provincias VALUES (6,'Provincia de Cachapoal',6);
+INSERT INTO Provincias VALUES (7,'Provincia de Talca',7);
+INSERT INTO Provincias VALUES (8,'Provincia de Concepcion',8);
+INSERT INTO Provincias VALUES (9,'Provincia de Cautin',9);
+INSERT INTO Provincias VALUES (10,'Provincia de Llanquihue',10);
+INSERT INTO Provincias VALUES (11,'Provincia de Coyhaique',11);
+INSERT INTO Provincias VALUES (12,'Provincia de Ultima Esperanza',12);
+INSERT INTO Provincias VALUES (13,'Provincia de Santiago',13);
+INSERT INTO Provincias VALUES (14,'Provincia de Valdivia',14);
+INSERT INTO Provincias VALUES (15,'Provincia de Arica',15);
+INSERT INTO Provincias VALUES (16,'Provincia de Punilla',16);
+
+
+INSERT INTO Comunas VALUES (1,'Comuna de Iquique',1);
+INSERT INTO Comunas VALUES (2,'Comuna de Antofagasta',2);
+INSERT INTO Comunas VALUES (3,'Comuna de Copiapo',3);
+INSERT INTO Comunas VALUES (4,'Comuna de La Serena',4);
+INSERT INTO Comunas VALUES (5,'Comuna de Viña del Mar',5);
+INSERT INTO Comunas VALUES (6,'Comuna de Rancagua',6);
+INSERT INTO Comunas VALUES (7,'Comuna de Talca',7);
+INSERT INTO Comunas VALUES (8,'Comuna de Concepcion',8);
+INSERT INTO Comunas VALUES (9,'Comuna de Temuco',9);
+INSERT INTO Comunas VALUES (10,'Comuna de Puerto Montt',10);
+INSERT INTO Comunas VALUES (11,'Comuna de Coyhaique',11);
+INSERT INTO Comunas VALUES (12,'Comuna de Punta Arenas',12);
+INSERT INTO Comunas VALUES (13,'Comuna de Santiago',13);
+INSERT INTO Comunas VALUES (14,'Comuna de Valdivia',14);
+INSERT INTO Comunas VALUES (15,'Comuna de Arica',15);
+INSERT INTO Comunas VALUES (16,'Comuna de San Carlos',16);
+
+INSERT INTO Sindicatos Values (1, 'SINTACT',12345678,'K');
+
+INSERT INTO Sedes VALUES (1,'Casa Central','Avenida Siempre Viva 123',13,1);
+
+INSERT INTO Aseguradoras VALUES (1,'MAPFRE',11111111,'1');
+INSERT INTO Aseguradoras VALUES (2,'SURA',22222222,'2');
+
+INSERT INTO Recepcionistas VALUES (1,'Miguel','','Bose','',11111111,'1',1);
+
+INSERT INTO Nacionalidades VALUES (1,'Chilena',0,'','');
+
+INSERT INTO Bancos VALUES (1,'Banco Estado');
+
+INSERT INTO Cheques VALUES (1,0);
+
+INSERT INTO Marcas VALUES (1,'Mazda');
+
+INSERT INTO Modelos VAlUES (1,'Mazda 3',1);
+
+INSERT INTO Tipo_Vehiculos VALUES (1,'Sedan');
+
+INSERT INTO Afiliados VALUES(1,'Joel','','Gutierrez','',912345678,'Rio Misisipi','Joelgutierrez@gmail.com',1,1);
+
+INSERT INTO Vehiculos VALUES (1,'GGWP69','Rojo','4G8P56JJ5LY825946','27JSN82750',1,1,1);
+
+INSERT INTO Tipo_Pagos Values(1,6000,0,'29/06/22','Pago Inicial: 6000 Pagado',1,1);
+
+INSERT INTO Beneficios VALUES (1,'Seguro Accidente',1,1);
+
+INSERT INTO Formas_Pago VALUES (1,'Efectivo',1,1);
+INSERT INTO Formas_Pago VALUES (2,'Transferencia',1,1);
+INSERT INTO Formas_Pago VALUES (3,'Cheque',1,1);
+
+INSERT INTO Estados VALUES (1,'Al dia','Aceptada',1);
+
+INSERT INTO Beneficios_Sindicato VALUES (1,'Usos: 0','Usos: 0','Usos: 0','Terrestre: Mes Junio',1,1);
+
+INSERT INTO Afiliados_Cargas VALUES (1,1);
+
+INSERT INTO Cargas VALUES (1,'Perro','','Jesus','','29/06/20','Mascota',1,1);
+
+INSERT INTO Solicitudes_Ingreso VALUES (1,'1','28/06/22',1500000,'Joel','','Gutierrez','',912345678,'Rio Misisipi','06/09/91','1','Joelgutierrez@gmail.com','Fluido','',1,1);
